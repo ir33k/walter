9a6a0397021fe3d3956916aa8651b47f20124454
irek <mail@gumen.pl>
2022-06-16 20:24:21 +0200

    Update demo0.t.c so it's more like a documentation

--- demo0.t.c
+++ demo0.t.c
@@ -1,7 +1,7 @@
 /* Simple example of everything you need to know. */
 
-/* Set max number of tests if you need more than default allows. */
-#define TESTMAX 1024
+/* Set max number of tests if you need more than 64. */
+#define TESTMAX 128
 #include "walter.h"
 
 TEST("All should pass")		/* Define test with assertions */
@@ -12,22 +12,20 @@ TEST("All should pass")		/* Define test with assertions */
 	char   buf[4]  = { '0', '0', '4', '5' };
 
 	OK(bool);		/* Is boolean true? */
+	ASSERT(bool, "text");	/* OK() with custom message */
 	EQ(num, 42);		/* Are numbers equal? */
 	STR_EQ(str, "David");	/* Are strings equal? */
 	BUF_EQ(buf, "0045", 4);	/* Are buffers equal? */
 
-	DIF(num, 22);	/* Negations */
-	STR_DIF(str, "Walter");
-	BUF_DIF(buf, "12345", 4);
-
-	/* ASSERT is like OK but with custom fail message. */
-	ASSERT(bool, "Fail message");
+	DIF(num, 22);		/* Are numbers different? */
+	STR_DIF(str, "Walter");	/* Are strings different? */
+	BUF_DIF(buf, "123", 3);	/* Are buffers different? */
 
 	END();			/* Force test to end here */
 	FAIL("Fail message");	/* Force fail in this line */
 }
 
-TEST("You shall not pass!")	/* 🧙 */
+TEST("You shall not pass!")	/* All should fail */
 {
 	OK(0);
 	EQ(0.1 + 0.2, 0.3);
@@ -89,4 +87,16 @@ TODO("Not finished or just ignored test") {
 	FAIL("Fail message");
 }
 
-/* There is no "main()" function. */
+/*
+ * There is no "main()" function.
+ */
+
+/* Compile and run:
+ *
+ *	$ cc -o demo0.t demo0.t.c   # Compile
+ *	$ ./demo0.t -h              # Print usage help
+ *	$ ./demo0.t                 # Run tests
+ *	$ ./demo0.t -v              # Run in verbose mode
+ *	$ ./demo0.t -q              # End quick on first fail
+ *	$ ./demo0.t -vq             # Captain obvious
+ */
